cec27a2fdfa8f65895619b6fa62c64eede092278
Lonnie King <lonnie.king@me.com>
2019-10-29 22:40:59 +1100

    font/style updates

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -13,8 +13,7 @@ module.exports = function layout(body) {
         <link rel="shortcut icon" type="image/png" href="${favicon}"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="${css}">
-        <meta name="google-site-verification" content="9PwAeGZ1oZvLcYctgYVy0wsxTfE0UvKgGqFq6idnTiY">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Corben|Open+Sans&display=swap" rel="stylesheet">
       </head>
       <body>
         ${body}
