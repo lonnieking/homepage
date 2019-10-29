9990b54bf1eb41952ab41fbf95dae4e80f9acd2b
Lonnie King <lonnie.king@me.com>
2019-10-29 23:18:47 +1100

    restyle with light/dark mode

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -6,17 +6,15 @@ exports.handler = async function http(request) {
   var brewersFriendUrl = 'https://www.brewersfriend.com/homebrew/brewer/176717/lonnieking'
 
   var body = `
-    <div class='introduction'>
-      Hi! I'm Lonnie!
-    </div>
-    <div class='content'>
-      <p>
-        I 'm a <a href=${githubUrl}>software developer</a>, currently working with the amazing folks at <a href='http://www.cogent.co'>Cogent</a>.
-      </p>
-      <p>
-        If you'd like to get into contact with me, please feel free to send me an <a href='mailto:hello@lonnie.cool'>electronic mail</a>.
-      </p>
-    </div>
+  <main>
+    <h1>Hi! I'm Lonnie!</h1>
+    <blockquote>
+      I'm a <a href=${githubUrl}>software developer</a>, currently working with the amazing folks at <a href='http://www.cogent.co'>Cogent</a>.
+    </blockquote>
+    <blockquote>
+      If you'd like to get into contact with me, please feel free to send me an <a href='mailto:hello@lonnie.cool'>electronic mail</a>.
+    </blockquote>
+  </main>
   `
   return {
     status: 201,
